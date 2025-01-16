9b9ffe2c499934b0e3e8e2007576a12e63b35de3
E-paivaa <United_hitz-1@hotmail.com>
2025-01-16 16:32:08 -0500

    Project 10 first submission

---
+++ hello-vite/src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import headerLogo from "../../assets/logo.svg";
+import headerAvatar from "../../assets/avatar.png";
+import "./Header.css";
+
+function Header({handleAddClick, weatherData}) {
+
+  const currentDate = new Date().toLocaleString('default', { month: 'long', day: 'numeric' });
+
+    return (
+      <div className="header">
+        <img className="header__logo" src={headerLogo} alt="Weather logo"/>
+        <p className="header__date-location">{currentDate}, {weatherData.city}  </p>
+        <button className="header__add-clothes" type="button" onClick={handleAddClick}> + Add clothes</button>
+        <div className="header__user">
+          <p className="header__username"> Edward Paiva</p>
+            <img src={headerAvatar} alt="avatar"className="header__avatar"/>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Header;
